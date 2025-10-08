675f1fb76441b3e345531dd805dc7ff449006fd2
harshlokhande103 <harshlokhande103@gmail.com>
2025-10-08 21:32:52 +0530

    add frontend link

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cookieParser())
 // CORS for frontend dev server (send cookies)
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://major-project-frontend-y7th.vercel.app'],
     credentials: true
   })
 )
